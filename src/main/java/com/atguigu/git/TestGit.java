3e37d0700fcc869cc035bd0d8e374246bfb90b86
dhbb09 <114057603+dhbb09@users.noreply.github.com>
2022-09-21 21:34:20 +0800

    pull test

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -6,7 +6,6 @@ package com.atguigu.git;
  **/
 public class TestGit{
     public static void main(String[] args) {
-        System.out.println("git~");
         System.out.println(111);
         System.out.println(222);
         System.out.println(333);
